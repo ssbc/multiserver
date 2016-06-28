4423537b797fca3e8c08a97f26c13b079539e536
Dominic Tarr <dominic.tarr@gmail.com>
2016-06-29 00:47:53 +1200

    implement support for transforms

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+'use strict'
+
+
 //create multiple types of server in one go.
 /*
   net-shs:
@@ -13,17 +16,44 @@
 
 */
 
+var compose = require('./compose')
+
+var isArray = Array.isArray
+function head (opts) {
+  return isArray(opts) ? opts[0] : opts
+}
+function tail (opts) {
+  return isArray(opts) ? opts.slice(1) : []
+}
+
+function compose (stream, transforms, cb) {
+  ;(function next (err, stream, i) {
+    if(err) return cb(err)
+    else if(i >= transforms.length) return cb(null, stream)
+    else
+      transforms[i](stream, function (err, stream) {
+        next(err, stream, i+1)
+      })
+  })(null, stream, 0)
+}
+
 function parse (u) {
-  var proto = u && 'object' == typeof u ? u : require('url').parse(u)
-  proto.protocol = proto.protocol.replace(/:$/, '')
-  return proto
+  return u && 'object' == typeof u ? u : require('url').parse(u)
 }
 
 module.exports = function (handlers) {
   function findProto (opts) {
     if(!opts) throw new Error('expected protocol descritpion')
     return handlers.find(function (e) {
-      return e.protocol == opts.protocol
+      var protocol = opts.protocol.replace(/:$/,'')
+      return e.protocol == protocol || e.name == protocol
+    })
+  }
+
+  function transforms (opts, isServer) {
+    return opts.map(function (opt) {
+      var create = findProto(opt) // create server???
+      return function (stream, cb) { create(stream, cb) }
     })
   }
 
@@ -31,20 +61,27 @@ module.exports = function (handlers) {
     createServer: function (opts, onConnection) {
       var closes = []
       opts.forEach(function (proto) {
-        var p = findProto(proto)
-        if(p) closes.push(p.createServer(proto, onConnection))
+        var p = findProto(head(proto))
+        if(p) closes.push(p.createServer(head(proto), function (stream) {
+          compose(stream, transforms(tail(proto), true), function (err, stream) {
+            if(err) console.error(err)
+            else onConnection(stream)
+          })
+        }))
       })
       return function () {
         closes.forEach(function (close) { close () })
       }
     },
     connect: function (address, cb) {
-      var p = findProto(parse(address))
-      if(p) p.connect(address, cb)
+      var opts = parse(address)
+      var p = findProto(head(opts))
+      if(p) p.connect(head(opts), function (err, stream) {
+        if(err) cb(err)
+        else compose(stream, transforms(tail(opts), false), cb)
+      })
       else cb(new Error('could not connect to:'+JSON.stringify(address)))
     }
   }
 }
-
-
 
