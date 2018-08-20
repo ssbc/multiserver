21025ae21a693b8ba05e9757a6bc1cdb08c492da
Anders Rune Jensen <arj03@protonmail.ch>
2018-08-21 00:04:58 +0200

    Make onion work again with new socks library

--- plugins/onion.js
+++ plugins/onion.js
@@ -1,86 +1,87 @@
-var socks = require('socks');
+var socks = require('socks').SocksClient;
 var toPull = require('stream-to-pull-stream')
 
 module.exports = function (opts) {
   if(!socks) { //we are in browser
-	console.warn('onion dialing through socks proxy not supported in browser setting')
-	return 
+    console.warn('onion dialing through socks proxy not supported in browser setting')
+    return 
   }
 
   opts = opts || {}
   var proxyOpts = {
-      ipaddress: "localhost",
-      //TODO: tor port should be configurable.
-      port: 9050, // default tor port
-      type: 5
+    ipaddress: "127.0.0.1",
+    //TODO: tor port should be configurable.
+    port: 9050, // default tor port
+    type: 5
   }
   return {
     name: 'onion',
     scope: function() { return opts.scope },
     server: function (onConnection) {
-          if(!opts.server) return
+      if(!opts.server) return
 
-          var serverOpts = {
-              proxy: proxyOpts,
-              command: "bind",
-              target: {
-                  host: opts.host,
-                  port: opts.port
-              }
-          }
-          var controlSocket = null
-          socks.createConnection(serverOpts, function (err, socket) {
-              if(err) {
-                console.error('unable to find local tor server.')
-				console.error('will be able receive tor connections') // << ???
-                return
-              }
-              controlSocket = socket
+      var serverOpts = {
+        proxy: proxyOpts,
+        command: "bind",
+        destination: {
+          host: opts.host,
+          port: opts.port
+        }
+      }
+      var controlSocket = null
+      socks.createConnection(serverOpts, function (err, socket) {
+        if(err) {
+          console.error('unable to find local tor server.')
+	  console.error('will be able receive tor connections') // << ???
+          return
+        }
+        controlSocket = socket
 
-              socket.on('data', function(stream) {
-                  stream = toPull.duplex(stream)
-                  stream.address = 'onion:'
-                  onConnection(stream)
-              })
+        socket.on('data', function(stream) {
+          stream = toPull.duplex(stream)
+          stream.address = 'onion:'
+          onConnection(stream)
+        })
 
-              // Remember to resume the socket stream.
-              socket.resume()
-          })
-          return function () {
-              if (controlSocket != null)
-                  controlSocket.end()
-          }
+        // Remember to resume the socket stream.
+        socket.resume()
+      })
+      return function () {
+        if (controlSocket != null)
+          controlSocket.end()
+      }
     },
     client: function (opts, cb) {
-        var started = false, _socket, destroy
+      var started = false, _socket, destroy
 
-        var connectOpts = {
-            proxy: proxyOpts,
-            command: "connect",
-            target: {
-                host: opts.host,
-                port: opts.port
-            }
+      var connectOpts = {
+        proxy: proxyOpts,
+        command: "connect",
+        destination: {
+          host: opts.host,
+          port: opts.port
         }
+      }
 
-        socks.createConnection(connectOpts, function(err, socket) {
-            if (err) return cb(err)
+      socks.createConnection(connectOpts, function(err, result) {
+        var socket = result.socket
+        if (err) return cb(err)
 
-            if(destroy) return socket.destroy()
-            _socket = socket
+        if(destroy) return socket.destroy()
+        _socket = socket
 
-            var duplexStream = toPull.duplex(socket)
-            duplexStream.address = 'onion:'+connectOpts.target.host+':'+connectOpts.target.port
+        var duplexStream = toPull.duplex(socket)
+        duplexStream.address = 'onion:'+connectOpts.destination.host+':'+connectOpts.destination.port
 
-            cb(null, duplexStream)
+        cb(null, duplexStream)
 
-            // Remember to resume the socket stream.
-            socket.resume()
-        })
-        return function () {
-          if(_socket) _socket.destroy()
-          else destroy = true
-        }
+        // Remember to resume the socket stream.
+        socket.resume()
+      })
+      return function () {
+        if(_socket) _socket.destroy()
+        else destroy = true
+      }
     },
     //MUST be onion:<host>:<port>
     parse: function (s) {
