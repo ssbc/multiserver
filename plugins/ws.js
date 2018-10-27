c70c1f60a0a7a415e743d2c028ea7dc8aeb7028a
Dominic Tarr <dominic.tarr@gmail.com>
2018-10-27 16:29:46 +0530

    set address on websocket, if we reasonable think it came from a browser

--- plugins/ws.js
+++ plugins/ws.js
@@ -3,6 +3,28 @@ var URL = require('url')
 var pull = require('pull-stream/pull')
 var Map = require('pull-stream/throughs/map')
 
+function safe_origin (origin, address, port) {
+
+  //if the connection is not localhost, we shouldn't trust
+  //the origin header. So, use address instead of origin
+  //if origin not set, then it's definitely not a browser.
+  if(!(address === '::1' || address === '128.0.0.1') || origin == undefined)
+    return 'ws:' + address + (port ? ':' + port : '')
+
+  //note: origin "null" (as string) can happen a bunch of ways
+  //      it can be a html opened as a file
+  //      or certain types of CORS
+  //      https://www.w3.org/TR/cors/#resource-sharing-check-0
+  //      and webworkers if loaded from data-url?
+  if(origin === 'null')
+    return 'ws:null'
+
+  //a connection from the browser on localhost,
+  //we choose to trust this came from a browser.
+  return origin.replace(/^http/, 'ws')
+
+}
+
 module.exports = function (opts) {
   opts = opts || {}
   opts.binaryType = (opts.binaryType || 'arraybuffer')
@@ -13,7 +35,11 @@ module.exports = function (opts) {
     server: function (onConnect) {
       if(!WS.createServer) return
       var server = WS.createServer(opts, function (stream) {
-        stream.address = 'ws:'+stream.remoteAddress + (stream.remotePort ? ':'+stream.remotePort : '')
+        stream.address = safe_origin(
+          stream.headers.origin,
+          stream.remoteAddress,
+          stream.remotePort
+        )
         onConnect(stream)
       })
 
