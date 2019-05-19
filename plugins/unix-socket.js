e72187f39671ac1aebd30d31915530458a6df3b5
Christian Bundy <christianbundy@fraction.io>
2019-05-19 11:38:13 -0700

    Use named pipe on Windows to unbreak build

--- plugins/unix-socket.js
+++ plugins/unix-socket.js
@@ -9,7 +9,12 @@ const os = require('os')
 var started = false
 
 module.exports = function (opts) {
-  opts.path = opts.path ||  fs.mkdtempSync(path.join(os.tmpdir(), 'multiserver-'))
+	if (process.platform === 'win32') {
+		opts.path = opts.path || path.join('\\\\?\\pipe', process.cwd(), 'multiserver')
+	} else {
+		opts.path = opts.path ||  fs.mkdtempSync(path.join(os.tmpdir(), 'multiserver-'))
+	}
+
   const socket = path.join(opts.path, 'socket')
   const addr = 'unix:' + socket
   let scope = opts.scope || 'device'
@@ -89,11 +94,16 @@ module.exports = function (opts) {
     //MUST be unix:socket_path
     parse: function (s) {
       var ary = s.split(':')
+
+      // Immediately return if there's no path.
       if(ary.length < 2) return null
+
+      // Immediately return if the first item isn't 'unix'.
       if('unix' !== ary.shift()) return null
+
       return {
         name: '',
-        path: ary.shift()
+        path: ary.join(':')
       }
     },
     stringify: function (_scope) {
